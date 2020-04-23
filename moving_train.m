5f32ac974e198f94d5c860ed291bc1498a885101
Zezhou Wang <wangzezai@hotmail.com>
2020-04-22 23:44:24 -0400

    Refactoring the file structrure, unzipping the raw data

--- moving_train.m
+++ moving_train.m
@@ -18,5 +18,7 @@ num1=xlsread('moving_train1.xlsx');
 X=num1(:,1);
 Y=num1(:,2);
 ans1=plot(X,Y,'-')
+xlim([0 10])
+ylim([-0.4 0.8])
 hold off
 legend([ans1, truevalue, truevalue1, truevalue2], 'Decawave', 'True Value', 'True Value with Buffer', 'True Value with Buffer');
