c116e714e982a9389cac9703a1c5a92a73c47e35
Sanyam Jain <jainsanyam786@gmail.com>
2020-06-26 05:44:33 -0400

    Added folder for two new experiments.
    Changes for new experiments are added. Also I have modified the buffer ploting logic from continous bar to single postion

--- UWB-Experiments-MATLAB/Geometry/GeometryExperiments.m
+++ UWB-Experiments-MATLAB/Geometry/GeometryExperiments.m
@@ -1,7 +1,9 @@
 %Ground truth
 X_TRUE = [0.22:0.29:2.25];
 Y_TRUE = repelem([0.55],length(X_TRUE));
-BUFFER_POS = [0.12, 0.45, 2.23, 0.2];
+%since we are ploting static postions it will be better we have separate
+%buffer for each postion
+%BUFFER_POS = [0.12, 0.45, 2.23, 0.2];
 
 %Anchor postion for TagElevated,AnchorsElevated,SquareTopology%
 x_anch_height=[0,0,2.33,2.33];
@@ -27,20 +29,22 @@ y_anch_exp4=[0,1.11,1.11,0];
 x_anch_exp4_geo2=[0,0,2.33,2.33,1];
 y_anch_exp4_geo2=[0,1.11,1.11,0,1.11];
 
-GeoExp('TagElevated',x_anch_height,y_anch_height,X_TRUE,Y_TRUE,BUFFER_POS);
-GeoExp('AnchorsElevated',x_anch_height,y_anch_height,X_TRUE,Y_TRUE,BUFFER_POS);
-GeoExp('SquareTopology',x_anch_height,y_anch_height,X_TRUE,Y_TRUE,BUFFER_POS);
-GeoExp('TriangleTopology',x_anch_tri,y_anch_tri,X_TRUE,Y_TRUE,BUFFER_POS);
-GeoExp('SingleSideOneAnchorBack',x_anch_one_side,y_anch_one_side,X_TRUE,Y_TRUE,BUFFER_POS);
-GeoExp('SingleSideTwoAnchorBack',x_anch_one_side_far,y_anch_one_side_far,X_TRUE,Y_TRUE,BUFFER_POS);
-GeoExp('TwoAnchorBlocked',x_anch_exp4,y_anch_exp4,X_TRUE,Y_TRUE,BUFFER_POS);
-GeoExp('TwoAnchorBlockedOneAdded',x_anch_exp4_geo2,y_anch_exp4_geo2,X_TRUE,Y_TRUE,BUFFER_POS);
-GeoExp('SquareTopology2',x_anch_height,y_anch_height,X_TRUE,Y_TRUE,BUFFER_POS);
-GeoExp('TagElevated2',x_anch_height,y_anch_height,X_TRUE,Y_TRUE,BUFFER_POS);
-GeoExp('TagAndAnchorElevated',x_anch_height,y_anch_height,X_TRUE,Y_TRUE,BUFFER_POS);
+GeoExp('TagElevated',x_anch_height,y_anch_height,X_TRUE,Y_TRUE);
+GeoExp('AnchorsElevated',x_anch_height,y_anch_height,X_TRUE,Y_TRUE);
+GeoExp('SquareTopology',x_anch_height,y_anch_height,X_TRUE,Y_TRUE);
+GeoExp('TriangleTopology',x_anch_tri,y_anch_tri,X_TRUE,Y_TRUE);
+GeoExp('SingleSideOneAnchorBack',x_anch_one_side,y_anch_one_side,X_TRUE,Y_TRUE);
+GeoExp('SingleSideTwoAnchorBack',x_anch_one_side_far,y_anch_one_side_far,X_TRUE,Y_TRUE);
+GeoExp('TwoAnchorBlocked',x_anch_exp4,y_anch_exp4,X_TRUE,Y_TRUE);
+GeoExp('TwoAnchorBlockedOneAdded',x_anch_exp4_geo2,y_anch_exp4_geo2,X_TRUE,Y_TRUE);
+GeoExp('SquareTopology2',x_anch_height,y_anch_height,X_TRUE,Y_TRUE);
+GeoExp('TagElevated2',x_anch_height,y_anch_height,X_TRUE,Y_TRUE);
+GeoExp('TagAndAnchorElevated',x_anch_height,y_anch_height,X_TRUE,Y_TRUE);
+GeoExp('AllAnchorsElevated',x_anch_height,y_anch_height,X_TRUE,Y_TRUE);
+GeoExp('AllAnchorsElevated2',x_anch_height,y_anch_height,X_TRUE,Y_TRUE);
 
 
-function GeoExp(name,x_anch_pos,y_anch_pos,x_true,y_true,buffer_pos)
+function GeoExp(name,x_anch_pos,y_anch_pos,x_true,y_true)
     % %Anchor Positions  for height exp
     cd (name)
     dinfo = dir('pos*.txt');
@@ -51,6 +55,7 @@ function GeoExp(name,x_anch_pos,y_anch_pos,x_true,y_true,buffer_pos)
     y_tag_pos_std=zeros(1,length(filenames));
     for K = 1 : length(filenames)
         thisfile = filenames{K};
+        display(thisfile);
         pos1 = readtable(thisfile);
         pos1 = pos1(:,1:2);
         pos1 = table2array(pos1);
@@ -64,15 +69,15 @@ function GeoExp(name,x_anch_pos,y_anch_pos,x_true,y_true,buffer_pos)
     end
 
 pos_plot(x_true, y_true, x_tag_pos_avg, y_tag_pos_avg, x_tag_pos_std,y_tag_pos_std,...
-    x_anch_pos, y_anch_pos, buffer_pos, name);
+    x_anch_pos, y_anch_pos, name);
 pos_errorbar(x_true, y_true, x_tag_pos_avg, y_tag_pos_avg, x_tag_pos_std,y_tag_pos_std,...
-    x_anch_pos, y_anch_pos, buffer_pos, name);
+    x_anch_pos, y_anch_pos, name);
 cd ..
 end
 
 
 function pos_plot(x_true, y_true, x_measure, y_measure, x_std, y_std, ...
-    x_anch, y_anch, buffer_pos, title_name)
+    x_anch, y_anch, title_name)
     % function call to plot according to different data
     % NO NEED TO COMMENT/UNCOMMENT
     % Display plot while done
@@ -87,7 +92,7 @@ function pos_plot(x_true, y_true, x_measure, y_measure, x_std, y_std, ...
     hold on;
     % Plot the dummy handles for legend
     std_1 = plot(nan, nan, 'bo', 'MarkerFaceColor','b');
-    buff = plot(nan, nan, 'LineStyle','--', 'Color','m','LineWidth',0.1);
+    %buff = plot(nan, nan, 'LineStyle','--', 'Color','m');
 
     % Plot the std. deviation for all data points of geo1
     for i = 1:1:length(x_true)
@@ -111,7 +116,10 @@ function pos_plot(x_true, y_true, x_measure, y_measure, x_std, y_std, ...
     % Plot the anchor positions
     anch = plot(x_anch, y_anch, 'b^');
     % Plot the buffer (+-10cm) for decawave
-    rectangle('Position', buffer_pos, 'LineStyle',':', 'EdgeColor','m', 'Curvature', 1,'LineWidth',0.3);
+    %rectangle('Position', buffer_pos, 'LineStyle',':', 'EdgeColor','m', 'Curvature', 1,'LineWidth',0.3);
+    centers = [x_true' y_true'];
+    radii = repelem(0.1,8,1);
+    buff=viscircles(centers,radii,'LineStyle','--','Color','m');
     
     if(contains(title_name,"Blocked"))
         rectangle('Position',BLOCKAGE1_POS, 'EdgeColor','k', 'FaceColor', 'k', 'Curvature', 0.2,'LineWidth',0.3);
@@ -137,7 +145,7 @@ function pos_plot(x_true, y_true, x_measure, y_measure, x_std, y_std, ...
 end
 
 function pos_errorbar(x_true, y_true, x_measure, y_measure, x_std, y_std, ...
-    x_anch, y_anch, buffer_pos, title_name)
+    x_anch, y_anch,title_name)
     % function call to plot according to different data, using errorbar
     % NO NEED TO COMMENT/UNCOMMENT
     % Display plot while done
@@ -155,9 +163,12 @@ function pos_errorbar(x_true, y_true, x_measure, y_measure, x_std, y_std, ...
     % Plot the anchor positions
     anch = plot(x_anch, y_anch, 'b^');
     % Plot the dummy handles for legend
-    buff = plot(nan, nan, 'LineStyle','--', 'Color','m','LineWidth',0.3);
+    %buff = plot(nan, nan, 'LineStyle','--', 'Color','m','LineWidth',0.3);
     % replot in a zoomed-in manner
-    rectangle('Position', buffer_pos, 'LineStyle',':', 'EdgeColor','m', 'Curvature', 1,'LineWidth',0.3);
+    %rectangle('Position', buffer_pos, 'LineStyle',':', 'EdgeColor','m', 'Curvature', 1,'LineWidth',0.3);
+    centers = [x_true' y_true'];
+    radii = repelem(0.1,8,1);
+    buff=viscircles(centers,radii,'LineStyle','--','Color','m');
     
     if(contains(title_name,"Blocked"))
         rectangle('Position',BLOCKAGE1_POS, 'EdgeColor','k', 'FaceColor', 'k', 'Curvature', 0.2,'LineWidth',0.3);
